b3def4fad2716fd53691396481628672c8ee5754
Thomas Sibley <trsibley@uw.edu>
2014-03-25 15:24:15 -0700

    Initial milla skeleton

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+App::RecordStream::Bio - Blah blah blah
+
+# SYNOPSIS
+
+    use App::RecordStream::Bio;
+
+# DESCRIPTION
+
+App::RecordStream::Bio is
+
+# AUTHOR
+
+Thomas Sibley <trsibley@uw.edu>
+
+# COPYRIGHT
+
+Copyright 2014- Thomas Sibley
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/App/RecordStream/Bio.pm
@@ -0,0 +1,39 @@
+package App::RecordStream::Bio;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::RecordStream::Bio - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::RecordStream::Bio;
+
+=head1 DESCRIPTION
+
+App::RecordStream::Bio is
+
+=head1 AUTHOR
+
+Thomas Sibley E<lt>trsibley@uw.eduE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Thomas Sibley
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
