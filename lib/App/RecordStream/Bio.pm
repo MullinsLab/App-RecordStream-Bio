fe5e8950cfc2eec9861f3f26218e338cb5567a9f
Thomas Sibley <trsibley@uw.edu>
2014-03-28 10:58:32 -0700

    Add example of recs-tofasta to primary dist doc

--- lib/App/RecordStream/Bio.pm
+++ lib/App/RecordStream/Bio.pm
@@ -20,6 +20,11 @@ App::RecordStream::Bio - A collection of record-handling tools related to biolog
     recs-fromfasta --oneline < seqs.fasta           \
         | recs-grep '{{id}} =~ /\b(POL|GAG)\b/i'    \
         | recs-tocsv -k id,sequence
+    
+    # Filter gaps from sequences
+    recs-fromfasta seqs.fasta \
+        | recs-xform '{{seq}} =~ s/-//g' \
+        | recs-tofasta > seqs-nogaps.fasta
 
 =head1 DESCRIPTION
 
@@ -32,6 +37,8 @@ them via their command line wrappers within a larger record stream pipeline.
 =head1 TOOLS
 
 L<recs-fromfasta>
+
+L<recs-tofasta>
 
 =head1 AUTHOR
 
